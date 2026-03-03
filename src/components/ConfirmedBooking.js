f35e81f4e1a5ca1b47dd21663e0dd7683603f862
lipnitzkyb <93211231+lipnitzkyb@users.noreply.github.com>
2026-03-02 21:56:35 -0600

    Update ConfirmedBooking.js

--- src/components/ConfirmedBooking.js
+++ src/components/ConfirmedBooking.js
@@ -1,7 +1,7 @@
 function ConfirmedBooking() {
   return (
     <section>
-      <h1>Booking Confirmed!</h1>
+      <h2>Booking Confirmed!</h2>
       <p>Your reservation has been successfully submitted.</p>
     </section>
   );
