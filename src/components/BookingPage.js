11cf64ea78bd88ef83e6cb34de98451f923b2b3b
lipnitzkyb <93211231+lipnitzkyb@users.noreply.github.com>
2026-03-02 21:56:11 -0600

    Update BookingPage.js

--- src/components/BookingPage.js
+++ src/components/BookingPage.js
@@ -3,7 +3,9 @@ import { fetchAPI, submitAPI } from "../api/bookingAPI";
 import BookingForm from "./BookingForm";
 import { useNavigate } from "react-router-dom";
 
-const updateTimes = (state, action) => {
+const initializeTimes = () => fetchAPI(new Date());
+
+export const updateTimes = (state, action) => {
   if (action.type === "UPDATE_TIMES") {
     return fetchAPI(action.date);
   }
@@ -11,7 +13,7 @@ const updateTimes = (state, action) => {
 };
 
 function BookingPage() {
-  const [availableTimes, dispatch] = useReducer(updateTimes, []);
+  const [availableTimes, dispatch] = useReducer(updateTimes, [], initializeTimes);
   const navigate = useNavigate();
 
   const submitForm = (formData) => {
@@ -22,7 +24,7 @@ function BookingPage() {
 
   return (
     <section aria-labelledby="booking-heading">
-      <h1 id="booking-heading">Reserve a Table</h1>
+      <h2 id="booking-heading">Reserve a Table</h2>
       <BookingForm
         availableTimes={availableTimes}
         dispatch={dispatch}
